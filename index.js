9e5b9778bdcb3810918a0e97bb8326150e834e15
nenedodo <nani.andres@hotmail.com.ar>
2022-08-29 20:04:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.set("port", process.env.PORT || 3001);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(app.get("port"), () =>
+  console.log("listening on port a" + app.get("port"))
+);
