13e45446532f7eb5b8bdc4459964488c39294eab
nenedodo <nani.andres@hotmail.com.ar>
2022-08-29 21:33:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.set("port", process.env.PORT || 3001);
+app.set('view engine', 'html');
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
